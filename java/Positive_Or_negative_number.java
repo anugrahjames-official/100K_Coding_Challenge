147a61cd2bcc040e5cc64b5ea4807bfefb79619f
Anugrah James <anugrahjames2006@gmail.com>
2024-03-29 18:45:34 +0530

    updates

--- java/Positive_Or_negative_number.java
+++ java/Positive_Or_negative_number.java
@@ -1,15 +1,18 @@
-public class IfSample {
-    Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
 
+public class IfSample {
+    
+    // Changed: Moved the declaration and initialization of Scanner object inside the main method
     public static void main(String[] args) {
+        // Scanner object declaration and initialization
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
 
-        if (num<0) {
+        if (num < 0) {
             System.out.println("Negative number");
         } else {
             System.out.println("Positive number");            
         }
-}
-
+    }
 }

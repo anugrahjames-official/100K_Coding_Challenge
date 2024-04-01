a5e04d626a224ef6746ad20dae89ced3d1941547
anugrahjames-official <anugrahjames2006@gmail.com>
2024-04-01 23:13:02 +0530

    Inheritance Contructucter

---
+++ java/InheritanceConstucter/src/A.java
@@ -0,0 +1,7 @@
+package InheritanceConstucter.src;
+
+public class A {
+    A() {
+        System.out.println("It's from Class A");
+    }
+}
---
+++ java/InheritanceConstucter/src/B.java
@@ -0,0 +1,7 @@
+package InheritanceConstucter.src;
+
+public class B extends A {
+    B() {
+        System.out.println("It's from Class B");
+    }
+}

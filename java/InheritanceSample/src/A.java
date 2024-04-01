bd8e35946e252ba494bca5a0ab7c9c2ad69c0556
anugrahjames-official <anugrahjames2006@gmail.com>
2024-04-01 23:04:45 +0530

    Inheritance Example

--- java/InheritanceSample/src/A.java
+++ java/InheritanceSample/src/A.java
@@ -1,5 +1,9 @@
 package InheritanceSample.src;
 
 public class A {
+    int a;
     
+    void displayA() {
+        System.out.println("Welcome to Class A");
+    }
 }
---
+++ java/InheritanceSample/src/B.java
@@ -0,0 +1,9 @@
+package InheritanceSample.src;
+
+public class B extends A {
+    int num;
+
+    void displayA() {
+        System.out.println("Welcome to Class B");       
+    }
+}

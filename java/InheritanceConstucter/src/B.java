f93991b1cc036fbc119a41e04473a48104db9903
anugrahjames-official <anugrahjames2006@gmail.com>
2024-04-03 13:52:53 +0530

    updates

--- java/InheritanceConstucter/src/B.java
+++ java/InheritanceConstucter/src/B.java
@@ -4,4 +4,8 @@ public class B extends A {
     B() {
         System.out.println("It's from Class B");
     }
+    public static void main(String[] args) {
+        B obj = new B();
+        
+    }
 }

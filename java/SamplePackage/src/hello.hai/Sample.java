7f8fd16b23e42f658d087a19ce5942593611459a
anugrahjames-official <anugrahjames2006@gmail.com>
2024-04-09 12:41:07 +0530

    Package

---
+++ java/SamplePackage/src/hello.hai/Sample.java
@@ -0,0 +1,7 @@
+package hello.hai;
+import hello.hey.Hello;
+public class Sample {
+    Sample() {
+        Hello h = new Hello();
+    }
+}
---
+++ java/SamplePackage/src/hello.hey/Hello.java
@@ -0,0 +1,3 @@
+package hello.hey;
+public class Hello {
+}
